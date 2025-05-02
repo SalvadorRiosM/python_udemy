00331c7ed8b3086fec4ec27d6342df6bf5c5326f
Salvador <salvadorriosmanzanales@gmail.com>
2025-05-01 20:49:21 -0600

    new repository

---
+++ list.py
@@ -0,0 +1,20 @@
+students_grades = [9.1, 8.8, 7.5]
+
+mysum = sum(students_grades)
+length = len(students_grades)
+
+promedy = mysum / length
+
+print(promedy)
+
+
+
+student_grades = [9.1, 8.8, 10.0, 7.7, 6.8, 8.0, 10.0, 8.1, 10.0, 9.9]
+
+items = len(student_grades)
+num_veces = student_grades.count(10.0)
+
+print(f"El 10.0 se repite {num_veces} veces")
+
+
+print(items)
