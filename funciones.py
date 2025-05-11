1f8231261cab6e083077015add7fcc49eef79099
Salvador <salvadorriosmanzanales@gmail.com>
2025-05-11 10:19:03 -0600

    modificar programa

--- funciones.py
+++ funciones.py
@@ -1,6 +1,6 @@
 def media(mylist):
     the_media = sum(mylist) / len(mylist)
-    return the_media
+    print(f"hola {the_media}")
    
-list_1 = [1,4,5]
-print(media(list_1))
+resultado = media([1,4,6])
+print(resultado)
